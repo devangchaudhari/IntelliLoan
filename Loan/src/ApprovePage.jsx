6478979a9e414aaa61843ddc05cd84940f5ac9a0
Devang Chaudhari <devangchaudhari2003@gmail.com>
2024-10-16 00:38:38 +0530

    change

--- Loan/src/ApprovePage.jsx
+++ Loan/src/ApprovePage.jsx
@@ -49,7 +49,7 @@ const ApprovePage = () => {
     }
 
     try {
-      const response = await fetch('https://intelliloan-python.vercel.app/predict', {
+      const response = await fetch('http://127.0.0.1:5000/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
